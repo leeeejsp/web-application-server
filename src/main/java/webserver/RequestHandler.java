cdb368a0b0e59095ddcc41463e029f3965442903
LEEjgsp <whdtjq1gh@naver.com>
2024-05-18 23:07:26 +0900

    /index.html 호출 시 html파일 불러오기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,9 +29,46 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            
+            // 입력값을 한 줄 단위로 읽기
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            
+            String line = br.readLine();
+            log.info("inputStream content : {}", line);
+            
+            
+            // 아무것도 입력하지 않은 경우
+        	String sep = File.separator;
+        	String Uri = String.join(sep, line.split(" ")[1].split("/")); // URI값
+        	log.info("URI : {}", Uri);
+        	
+        	
+        	if(Uri.equals("")) {
+        		byte[] body = "Hello World".getBytes();
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+                return;
+        	}
+        	
+        	
+        	// 뭐라도 입력한 경우
+        	String filePath = "." + sep + "webapp" + Uri;
+        	log.info("filePath : {}", filePath);
+            	
+        	
+        	File file = new File(filePath);
+        	if(file.exists()) {
+        		byte[] body = Files.readAllBytes(file.toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+                return;
+        	}else {
+        		// 입력한 url의 리소스가 존재하지 않는 경우
+              byte[] body = "404 error".getBytes();
+              response200Header(dos, body.length);
+              responseBody(dos, body);
+        	}
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
